1a11136c7cdcc94e4ea381b13c91ff34a0241888
CrazyWonton <kevintarc13@gmail.com>
2017-12-10 00:48:34 -0500

    commented it

--- AStar.java
+++ AStar.java
@@ -3,16 +3,26 @@ import java.io.*;
 import java.util.*;
 
 /**
-Purpose: 
-Methods:
+Purpose: Container class for all nested classes. Runs A* and Dijkstras
+	algorithm on generated graphs. Writes time taken for both to
+	a file.
+Methods:	setBlocked
+		setStartCell
+		setEndCell
+		checkAndUpdateCost
+		AStar
+		test
+		generateStuff
+		main
 **/
 public class AStar {
 	public static final int DIAGONAL_COST = 14;
 	public static final int V_H_COST = 10;
 	
 	/**
-	Purpose: 
-	Methods:
+	Purpose: Holds information on each node in the graph 
+	Methods: 	Cell
+			toString
 	**/
 	static class Cell{  
 		int heuristicCost = 0; //Heuristic cost
@@ -21,9 +31,10 @@ public class AStar {
 		Cell parent; 
 		
 		/**
-		Purpose: 
-		How it works:
-		Parameters:
+		Purpose: constructor
+		How it works: accepts two perametrs and sets them to
+			the appropriate values
+		Parameters:int i and int j
 		**/
 		Cell(int i, int j){
 			this.i = i;
@@ -31,9 +42,9 @@ public class AStar {
 		}
 
 		/**
-		Purpose: 
-		How it works:
-		Parameters:
+		Purpose: overrides toString method for output
+		How it works: overrides toString
+		Parameters: void
 		**/
 		@Override
 		public String toString(){
@@ -51,35 +62,36 @@ public class AStar {
 	static int endI, endJ;
 
 	/**
-	Purpose: 
-	How it works:
-	Parameters:
+	Purpose: set blocked nodes in graph
+	How it works: sets a value to null
+	Parameters: coordinates int i and int j
 	**/
 	public static void setBlocked(int i, int j){
 		grid[i][j] = null;
 	}
 	/**
-	Purpose: 
-	How it works:
-	Parameters:
+	Purpose: sets start cell of search
+	How it works: sets perameters to input
+	Parameters: coordinates int i and int j
 	**/
 	public static void setStartCell(int i, int j){
 		startI = i;
 		startJ = j;
 	}
 	/**
-	Purpose: 
-	How it works:
-	Parameters:
+	Purpose: set destination cell
+	How it works:sets two values given input
+	Parameters: coordinates i and j
 	**/
 	public static void setEndCell(int i, int j){
 		endI = i;
 		endJ = j; 
 	}
 	/**
-	Purpose: 
-	How it works:
-	Parameters:
+	Purpose: attempts to add cell current to cell t
+	How it works: if statements check for conditions
+		adds it to path if it can
+	Parameters: cells current and t, int cost
 	**/
 	static void checkAndUpdateCost(Cell current, Cell t, int cost){
 		if(t == null || closed[t.i][t.j])return;
@@ -93,9 +105,10 @@ public class AStar {
 		}
 	}
 	/**
-	Purpose: 
-	How it works:
-	Parameters:
+	Purpose: Static class to run instances of A*
+	How it works: Create an instance of A* and run it
+		with the main method
+	Parameters: void
 	**/
 	public static void AStar(){ 
 
@@ -156,9 +169,9 @@ public class AStar {
 		} 
 	}
 	/**
-	Purpose: 
-	How it works:
-	Parameters:
+	Purpose: generates a graph and tests A* and Dijkstras algorithm on it
+	How it works: randomly generates a graph and runs both algorithms on it
+	Parameters: test case #, size x, size y, start i, start j, end i, end j, matrix of blocked nodes
 	**/
 	public static long test(int tCase, int x, int y, int si, int sj, int ei, int ej, int[][] blocked){
 		//System.out.println("\n\nTest Case #"+tCase);
@@ -219,9 +232,9 @@ public class AStar {
 	static int average = 0;
 	static int numTrials = 0;
 	/**
-	Purpose: 
-	How it works:
-	Parameters:
+	Purpose: generates a randomized graph for the algorithms to run on 
+	How it works: using random numbers, creates a graph
+	Parameters: size x, size y
 	**/
 	public static void generateStuff(int x, int y){
 		//int casenum;
@@ -259,40 +272,43 @@ public class AStar {
 
 	public static LinkedList<timeTaco> mayo;
 	/**
-	Purpose: 
-	Methods:
+	Purpose:  container to hold times for each run
+	Methods:	timeTaco
+			toString
 	**/
 	public static class timeTaco{
 		long d;
 		long a;
 		/**
-		Purpose: 
-		How it works:
-		Parameters:
+		Purpose: create an instance of timeTaco
+		How it works: N/A
+		Parameters: time for dijkstras, time for A*
 		**/
 		public timeTaco(long dd,long aa){
 			d = dd;
 			a = aa;
 		}
 		/**
-		Purpose: 
-		How it works:
-		Parameters:
+		Purpose: format output
+		How it works: override toString
+		Parameters: void
 		**/
 		public String toString(){
 			return a + ", " + d;
 		}
 	}
 	/**
-	Purpose: 
-	Methods:
+	Purpose: nested class to section off computations for dijkstras
+	Methods: 	minDistance
+			dijkstra
+			convertInput
 	**/
 	public static class dij{
 		static final int V=9;
 		/**
-		Purpose: 
-		How it works:
-		Parameters:
+		Purpose: find the min distance
+		How it works: iterates through the array
+		Parameters: int[] and boolean[]
 		**/
 		int minDistance(int dist[], Boolean sptSet[]){
 			int min = Integer.MAX_VALUE, min_index=-1;
@@ -307,9 +323,9 @@ public class AStar {
 			return min_index;
 		}
 		/**
-		Purpose: 
-		How it works:
-		Parameters:
+		Purpose: run dijsktras algorithm on the graph
+		How it works: uses dijkstras algorithm to compute min distance
+		Parameters: int graph[][] and int src
 		**/
 		void dijkstra(int graph[][], int src)
 		{
@@ -340,9 +356,9 @@ public class AStar {
 			}
 		}
 		/**
-		Purpose: 
-		How it works:
-		Parameters:
+		Purpose: converts graph data from A* class to input for dijkstras
+		How it works: interprets a graph and passes it along
+		Parameters: int x, int y, and int[][]
 		**/
 		public long convertInput(int x, int y, int[][] blocked){
 			int graph [][] = new int[x][y];
@@ -365,9 +381,9 @@ public class AStar {
 		}
 	}
 	/**
-	Purpose: 
-	How it works:
-	Parameters:
+	Purpose: driver method
+	How it works: runs the proper algorithms and writes them to a file
+	Parameters: args[]
 	**/
 	public static void main(String[] args) throws IOException{   
 		mayo = new LinkedList<>();
